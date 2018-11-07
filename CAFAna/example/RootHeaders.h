dbc4d96a3ed2691ddd9cd338491286b4696a9511
Sebastian Sanchez <sebastian.sanchez@pucp.edu.pe>
2018-11-07 16:35:36 -0600

    Create RootHeaders.h

---
+++ CAFAna/example/RootHeaders.h
@@ -0,0 +1,63 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <TRandom.h>
+#include <TRandom3.h>
+#include <TH1D.h>
+#include <TH1F.h>
+#include <TH2.h>
+#include <TH2F.h>
+#include <TH2D.h>
+#include <TROOT.h>
+#include <TStyle.h>
+#include <TSystem.h>
+#include <TVector3.h>
+#include <TVectorD.h>
+#include <TCanvas.h>
+#include <TFile.h>
+#include <TSpectrum.h>
+#include <TF1.h>
+#include <TF2.h>
+#include <TH1.h>
+#include <TGraph.h>
+#include <TGraphErrors.h>
+#include <TGraphAsymmErrors.h>
+#include <TGraph2D.h>
+#include <TLatex.h>
+#include <TLegend.h>
+#include <TGaxis.h>
+#include <TBox.h>
+#include <TText.h>
+#include <TTree.h>
+#include <TLeaf.h>
+#include <TMinuit.h>
+#include <TVirtualFitter.h>
+#include <TMatrixD.h>
+#include <TNtuple.h>
+#include <TNtupleD.h>
+#include <TDecompChol.h>
+#include <TMatrixDSym.h>
+#include <TMath.h>
+#include <TChain.h>
+#include <TProfile2D.h>
+#include <TColor.h>
+#include "TROOT.h"
+#include "TFitResult.h"
+#include "TKey.h"
+#include "THStack.h"
+
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <math.h>
+#include <cstdlib>
+#include <algorithm>
+
+using namespace std;
+
+#endif
